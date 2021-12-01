b8444d639c2aad69f29fa96630b3fddba0ce3fda
Hiago-Araujo <44373223+Hiago-Araujo@users.noreply.github.com>
2021-12-01 13:31:04 -0300

    Update app.py

--- Codigo_Bomba_cav/app.py
+++ Codigo_Bomba_cav/app.py
@@ -137,7 +137,7 @@ if img_file_buffer is not None:
         st.image(np.array(mpimg.imread(wd+'icons/critico.png')))
     
 st.subheader("Imagem de exemplo, baixe e envie para testar a previsão do modelo")
-agree = st.button("Manda")
+agree = st.checkbox("Manda")
 
 if agree:
     exemplo = random.choice(orig)
